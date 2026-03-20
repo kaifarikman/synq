4f453746abfa41a1d0c6a9cd579461a0659e5e23
sptrent <artemrazumovvv@gmail.com>
2026-03-20 15:21:21 +0300

    feat(backend): Add Account domain, ORM-model Users and AccountRepositoryProtocol
    Create Account and AccountRepositoryProtocol in authentification/domain/
    Create ORM-model User in authentification/infrastructure and create table in Postgres with Alembic

--- backend/authentication/domain/repositories.py
+++ backend/authentication/domain/repositories.py
@@ -1,1 +1,11 @@
 """Абстракция репозитория"""
+
+from typing import Protocol
+
+from .entities import Account
+
+
+class AccountRepositoryProtocol(Protocol):
+    def get_account_by_id(self, id: int) -> Account | None: ...
+    def get_account_by_email(self, email: str) -> Account | None: ...
+    def add_account(self, account: Account) -> None: ...
