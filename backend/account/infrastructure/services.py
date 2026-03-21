0515023f6cac83c7af2c89041d3cdd3c84806f75
Artem <artemrazumovvv@gmail.com>
2026-03-21 12:10:25 +0300

    Add PasswordServide protocol and BcryptPasswordService (#31)

--- backend/account/infrastructure/services.py
+++ backend/account/infrastructure/services.py
@@ -1,1 +1,21 @@
 """Отвечает за технические сервисы контекста"""
+
+import bcrypt
+
+
+class BcryptPasswordService:
+    @staticmethod
+    def hash_password(password: str) -> str:
+        return (
+            bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        ).decode()
+
+    @staticmethod
+    def check_password(password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(
+            password.encode('utf-8'), hashed_password.encode('utf-8')
+        )
+
+
+class JWTService:
+    pass
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,9 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from account.infrastructure.models import UserModel  # noqa: F401
 from app.config import settings
 from app.db import Base
-from account.infrastructure.models import UserModel  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
