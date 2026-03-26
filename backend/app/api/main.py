3cb97839ffa44fa6bfc5fa655e4dcdef04c51205
kaifarikman <kna.zevs@yandex.ru>
2026-03-26 19:29:54 +0300

    chore(dev): enable frontend-backend local integration

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,7 @@
 """Отвечает за запуск FastAPI-приложения и подключение роутов"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import app.db_registry
 from app.api.auth_routes import auth
@@ -8,5 +9,15 @@ from app.api.profile_routes import profile
 from app.config import settings
 
 app = FastAPI(title='SYNQ')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+    ],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 app.include_router(auth, prefix=settings.api_prefix)
 app.include_router(profile, prefix=settings.api_prefix)
